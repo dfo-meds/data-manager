4f40189c134c80c4a34b85de32cdc98218ee930d
Erin Turnbull <erin.turnbull@dfo-mpo.gc.ca>
2023-02-28 16:25:56 -0500

    Added some language caching for speed

--- src/pipeman/builtins/i18n_yaml/translator.py
+++ src/pipeman/builtins/i18n_yaml/translator.py
@@ -3,6 +3,7 @@ import os
 from autoinject import injector
 from pipeman.i18n import LanguageDetector
 import zirconium as zr
+import logging
 
 
 class YamlTranslationManager:
@@ -14,14 +15,19 @@ class YamlTranslationManager:
     def __init__(self, dictionary_path: str = None):
         if dictionary_path is None:
             dictionary_path = self.cfg.as_path(('pipeman', 'i18n_yaml', 'dictionary_path'))
+        self.log = logging.getLogger("pipeman.i18n_yaml")
+        self._warn_on_missing_key = self.cfg.as_bool(('pipeman', 'i18n_yaml', 'warn_missing_key'), default=False)
         self._dictionaries = {}
         self._dictionary_lookup = {}
         for file in os.scandir(dictionary_path):
             if file.name.endswith(".yaml") or file.name.endswith(".yml"):
                 self._dictionary_lookup[file.name[:file.name.rfind(".")]] = file.path
+        self._supported = None
 
     def supported_languages(self):
-        return self._dictionary_lookup.keys()
+        if not self._supported:
+            self._supported = list(self._dictionary_lookup.keys())
+        return self._supported
 
     def _ensure_dictionary(self, lang):
         if lang not in self._dictionaries:
@@ -33,8 +39,11 @@ class YamlTranslationManager:
     def get_text(self, text_key: str, default: str = None) -> str:
         lang = self.ld.detect_language(self.supported_languages())
         if not lang:
+            self.log.error(f"Could not find a supported language, options {'|'.join(self.supported_languages())}")
             raise ValueError("Could not agree on a language")
         self._ensure_dictionary(lang)
         if text_key not in self._dictionaries[lang]:
+            if self._warn_on_missing_key:
+                self.log.warning(f"Missing language key {text_key} for {lang}")
             return text_key if default is None else default
         return self._dictionaries[lang][text_key]
