b0cb36ad5e158dd37f25b8c7f6788ecf505909b8
Erin Turnbull <erin.turnbull@dfo-mpo.gc.ca>
2023-03-15 17:12:52 -0400

    Added DB upgrade command

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,8 @@ set -e
 
 cd /srv/metadb/app || exit
 
+python -m alembic upgrade head
+
 python cli.py core setup
 
 python cli.py user create --display "Administrator" --no-error --password "${METADB_PASSWORD:-modernmajorgeneral}" "${METADB_USERNAME:-admin}" "${METADB_EMAIL:-meds.erddap@gmail.com}"
