5e9d6114d2bca867666684346411ad7573ccefcc
Erin Turnbull <erin.turnbull@dfo-mpo.gc.ca>
2023-03-16 14:13:57 -0400

    Fixed bug in group assign for admin user

--- docker/start.sh
+++ docker/start.sh
@@ -9,7 +9,7 @@ python cli.py core setup
 
 python cli.py user create --display "Administrator" --no-error --password "${METADB_PASSWORD:-modernmajorgeneral}" "${METADB_USERNAME:-admin}" "${METADB_EMAIL:-meds.erddap@gmail.com}"
 
-python cli.py user group --no-error assign "${METADB_USERNAME:-admin}" superadmin
+python cli.py user group assign --no-error "${METADB_USERNAME:-admin}" superadmin
 
 # If there's a prestart.sh script in the /app directory, run it before starting
 PRE_START_PATH=/srv/metadb/prestart.sh
