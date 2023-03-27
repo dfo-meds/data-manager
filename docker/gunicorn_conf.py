01408a62f1689c71b3cfa47e83737a88bbfad7dc
Erin Turnbull <erin.turnbull@dfo-mpo.gc.ca>
2023-03-27 11:58:30 -0400

    better gunicorn configuration

--- docker/gunicorn_conf.py
+++ docker/gunicorn_conf.py
@@ -4,62 +4,61 @@ import json
 import multiprocessing
 import os
 
-"""""
-workers_per_core_str = os.getenv("WORKERS_PER_CORE", "2")
+# Load defaults from environment
+workers_per_core_str = int(os.getenv("WORKERS_PER_CORE", 2))
 web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
 worker_class = os.getenv("WORKER_CLASS", "gevent")
-worker_connections = os.getenv("WORKER_CONNECTIONS", 1000)
-max_requests = os.getenv("WORKER_MAX_REQUESTS", 5000)
-max_requests_jitter = 10
+worker_connections = int(os.getenv("WORKER_CONNECTIONS", 1000))
+max_requests = int(os.getenv("WORKER_MAX_REQUESTS", 5000))
+max_requests_jitter = int(os.getenv("WORKER_MAX_REQUESTS_JITTER", 10))
+timeout = int(os.getenv("REQUEST_TIMEOUT", 30))
+keepalive = int(os.getenv("KEEPALIVE", 15))
 host = os.getenv("HOST", "0.0.0.0")
 port = os.getenv("PORT", "80")
 bind_env = os.getenv("BIND", None)
-use_loglevel = os.getenv("LOG_LEVEL", "info")
-if bind_env:
-    use_bind = bind_env
-else:
-    use_bind = "{host}:{port}".format(host=host, port=port)
+errorlog = os.getenv("ERROR_LOG", "-")
+accesslog = os.getenv("ACCESS_LOG", "")
+loglevel = os.getenv("LOG_LEVEL", "info")
+enable_stdio_inheritance = os.getenv("ENABLE_STDIO_INHERITANCE", "1") == "1"
 
-enable_stdio_inheritance=True
+# Set use_bind
+if bind_env:
+    bind = bind_env
+else:
+    bind = "{host}:{port}".format(host=host, port=port)
 
 cores = multiprocessing.cpu_count()
-workers_per_core = float(workers_per_core_str)
-default_web_concurrency = (workers_per_core * cores) + 1
+# Set number of workers as needed
 if web_concurrency_str:
-    web_concurrency = int(web_concurrency_str)
-    assert web_concurrency > 0
+    workers = int(web_concurrency_str)
 else:
-    web_concurrency = int(default_web_concurrency)
+    workers_per_core = float(workers_per_core_str)
+    default_web_concurrency = (workers_per_core * cores) + 1
+    workers = int(default_web_concurrency)
+assert workers > 0
+assert worker_class in ('sync', 'eventlet', 'gevent', 'tornado', 'gthread')
 
-# Errors in the threaded workers we wil lhave to fix later
-web_concurrency = 1
-worker_class = "sync"
-"""
-# Gunicorn config variables
-loglevel = "info"
-bind = "0.0.0.0:80"
-#keepalive = 120
-errorlog = "-"
-#accesslog = "-"
-#keepalive = 15
-web_concurrency = 1
-workers = 1
-worker_class = "sync"
-
-
-# For debugging and testing
-log_data = {
-    "loglevel": loglevel,
-    "workers": web_concurrency,
-    "worker_class": worker_class,
-#    "worker_connections": worker_connections,
-#    "max_requests": max_requests,
-#    "max_requests_jitter": max_requests_jitter,
-#    "keepalive": keepalive,
-    "bind": bind,
-    # Additional, non-gunicorn variables
-#    "workers_per_core": workers_per_core,
-#    "host": host,
-#    "port": port,
-}
-print(json.dumps(log_data))
+if os.getenv("DUMP_GUNICORN_CONFIG", "1") == "1":
+    # For debugging and testing
+    log_data = {
+        "loglevel": loglevel,
+        "workers": workers,
+        "worker_class": worker_class,
+        "worker_connections": worker_connections,
+        "max_requests": max_requests,
+        "max_requests_jitter": max_requests_jitter,
+        "keepalive": keepalive,
+        "bind": bind,
+        "enable_stdio_inheritance": enable_stdio_inheritance,
+        "accesslog": accesslog,
+        "errorlog": errorlog,
+        "timeout": timeout,
+        "_host": host,
+        "_port": port,
+        "_fixed_web_concurrency": web_concurrency_str,
+        "_cpu_count": cores,
+        "_workers_per_core": workers_per_core_str,
+    }
+    print("gunicorn config::")
+    for x in log_data:
+        print(f"{x: ^20}{log_data[x]}")
--- src/pipeman/init/__init__.py
+++ src/pipeman/init/__init__.py
@@ -5,36 +5,28 @@ import os
 import logging
 
 
-def _cfg_files_from_template(filename):
-    custom_config_path = os.environ.get("PIPEMAN_CONFIG_DIR", "./config")
-    logging.getLogger().warning(f"Loading extra config from {custom_config_path}")
+def _config_paths():
+    yield pathlib.Path(".").absolute()
+    yield pathlib.Path("~").absolute().expanduser()
+    custom_config_path = os.environ.get("PIPEMAN_CONFIG_SEARCH_PATHS", "./config")
     if custom_config_path:
-        p = pathlib.Path(custom_config_path)
-        if p.exists():
-            yield str(p / filename)
-        else:
-            print(f"Missing custom config directory {custom_config_path}")
-    yield f"~/{filename}"
-    yield f"./{filename}"
-
-
-@zr.configure
-def configure_pipeman(config: zr.ApplicationConfig):
-    me = pathlib.Path(__file__).absolute()
-    config.register_default_file(me.parent / ".pipeman.defaults.toml")
-    for file in _cfg_files_from_template(".pipeman.defaults.toml"):
-        config.register_default_file(file)
-    for file in _cfg_files_from_template(".pipeman.toml"):
-        config.register_file(file)
+        paths = custom_config_path.split(";")
+        for path in paths:
+            p = pathlib.Path(path)
+            if p.exists():
+                yield p
 
 
 def init(db_only=False, extra_files=None):
 
     @zr.configure
     def configure_extra_files(config: zr.ApplicationConfig):
-        for file in extra_files or []:
-            for cfg_file in _cfg_files_from_template(file):
-                config.register_file(cfg_file)
+        config_paths = [x for x in _config_paths()]
+        for path in config_paths:
+            config.register_default_file(path / ".pipeman.defaults.toml")
+            config.register_file(path / ".pipeman.toml")
+            for filename in extra_files or []:
+                config.register_file(path / filename)
 
     from pipeman.util.system import System
 
