10a6935d1033bbac86673f041e7cc914ba58402b
Erin Turnbull <erin.turnbull@dfo-mpo.gc.ca>
2023-03-27 11:02:48 -0400

    easier startup?

--- docker/build.sh
+++ docker/build.sh
@@ -5,11 +5,3 @@ PIPEMAN_CONFIG_DIR=/metadb-config
 export PIPEMAN_CONFIG_DIR
 
 cd /srv/metadb/app || exit
-
-python -m alembic upgrade head
-
-python cli.py core setup
-
-python cli.py user create --display "Administrator" --no-error --password "${METADB_PASSWORD:-modernmajorgeneral}" "${METADB_USERNAME:-admin}" "${METADB_EMAIL:-meds.erddap@gmail.com}"
-
-python cli.py user group assign --no-error "${METADB_USERNAME:-admin}" superadmin
