b13b8f8c31e13242b6109cb584f8030cf53400b2
Erin Turnbull <erin.a.turnbull@gmail.com>
2023-07-12 12:05:20 -0400

    better attempt to send blank body for compilation request

--- src/pipeman/plugins/cnodc/workflow.py
+++ src/pipeman/plugins/cnodc/workflow.py
@@ -1,5 +1,4 @@
-import base64
-
+import zrlog
 import zirconium as zr
 from autoinject import injector
 import requests
@@ -8,6 +7,7 @@ import base64
 
 @injector.inject
 def notify_erddaputil_http(step, context, cfg: zr.ApplicationConfig = None):
+    log = zrlog.get_logger("pipeman.cnodc")
     erddap_cluster = step.item_config["erddap_cluster"]
     notify_endpoint = cfg.as_str(("cnodc", "erddaputil", erddap_cluster, "notify_base_url"), default=None)
     username = cfg.as_str(("cnodc", "erddaputil", erddap_cluster, "username"), default=None)
@@ -25,17 +25,22 @@ def notify_erddaputil_http(step, context, cfg: zr.ApplicationConfig = None):
         notify_endpoint += "/"
     notify_endpoint += "datasets/compile"
     unpwd = f"{username}:{password}"
-    resp = requests.post(
-        url=notify_endpoint,
-        headers={
-            "Authorization": f"basic {base64.b64encode(unpwd.encode('utf-8')).decode('ascii')}",
-            "Content-Type": "application/json"
-        }
-    )
-    j = resp.json()
-    if j and "message" in j:
-        if isinstance(j["message"], list):
-            step.output.extend(j["message"])
-        else:
-            step.output.append(j["message"])
-    resp.raise_for_status()
+    try:
+        body = {}
+        resp = requests.post(
+            url=notify_endpoint,
+            headers={
+                "Authorization": f"basic {base64.b64encode(unpwd.encode('utf-8')).decode('ascii')}"
+            },
+            json=body
+        )
+        j = resp.json()
+        if j and "message" in j:
+            if isinstance(j["message"], list):
+                step.output.extend(j["message"])
+            else:
+                step.output.append(j["message"])
+        resp.raise_for_status()
+    except Exception as ex:
+        log.notice(f"Request to POST {notify_endpoint} failed, body: [{body}]")
+        raise ex
