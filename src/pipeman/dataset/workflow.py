9442398b34ed911b02e1250aa65872fa6335906e
Erin Turnbull <erin.a.turnbull@gmail.com>
2023-07-13 15:31:21 -0400

    work on dataset action notifications

--- src/pipeman/dataset/workflow.py
+++ src/pipeman/dataset/workflow.py
@@ -4,6 +4,11 @@ import pipeman.db.orm as orm
 from pipeman.workflow import ItemResult
 import datetime
 import zrlog
+from pipeman.email import EmailController
+from pipeman.dataset import DatasetController
+import typing as t
+
+from pipeman.workflow.steps import WorkflowStep
 
 
 @injector.inject
@@ -72,3 +77,28 @@ def return_to_draft(step, context, db: Database = None):
         session.commit()
         log.info(f"Dataset [{context['dataset_id']}] returned to draft")
         return ItemResult.SUCCESS
+
+
+@injector.inject
+def send_dataset_action_email(step: WorkflowStep, context: dict, emails: EmailController):
+    by_lang_pref = _email_list_for_step(step, context)
+    for lang_pref in by_lang_pref:
+        emails.send_template(
+            step.item_config['email_template'],
+            lang_pref,
+            to_emails=by_lang_pref[lang_pref],
+            immediate=True
+        )
+
+
+@injector.inject
+def _email_list_for_step(step: WorkflowStep, context: dict, dc: DatasetController) -> t.Mapping[str, t.List[str]]:
+    by_lang_pref = {}
+    # TODO
+    #for user in _email_list_for_step(step, context):
+    #   if user.language_preference not in by_lang_pref:
+    #        by_lang_pref[user.language_preference] = []
+    #    by_lang_pref[user.language_preference].append(user.email)
+    return by_lang_pref
+
+
